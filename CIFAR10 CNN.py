f36036d2105e843422dc871c83657bc2f020e44c
Ivan-Sotelo <48335281+Ivan-Sotelo@users.noreply.github.com>
2019-03-08 23:41:07 +0800

    Add files via upload

--- CIFAR10 CNN.py
+++ CIFAR10 CNN.py
@@ -7,9 +7,12 @@ import numpy as np
 from keras.datasets import cifar10
 from keras.utils.np_utils import to_categorical
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import Adam
+from keras.layers import Dense, Activation, Dropout, Flatten
+from keras.optimizers import SGD
+from keras.layers import Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D
 import matplotlib.pyplot as plt
+from keras.layers.normalization import BatchNormalization
 
 
 #Loading the dataset
@@ -19,32 +22,61 @@ import matplotlib.pyplot as plt
 y_train = to_categorical(y_train, num_classes=10)
 y_test = to_categorical(y_test, num_classes=10)
 
-#Image to Vector
-X_train = np.reshape(X_train,(50000,3072))
-X_test = np.reshape(X_test,(10000,3072))
-
 #normalization [0,1]
 X_train = X_train.astype('float32')/255
 X_test = X_test.astype('float32')/255
 
-#3 layer MLP
+
+
+print("Shape of training data:")
+print(X_train.shape)
+print(y_train.shape)
+print("Shape of test data:")
+print(X_test.shape)
+print(y_test.shape)
+
+
+
+
+#3 layer CNN model
 model = Sequential()
-model.add(Dense(1024, activation='relu', input_dim=3072))
+
+model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3)))
+
+#Batch Normalization Layer
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Dropout(0.15))
+
+model.add(Conv2D(32, (3, 3)))
+
+#Batch Normalization Layer
+model.add(BatchNormalization())
+
+model.add(Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+#Dropout layer
 model.add(Dropout(0.25))
-model.add(Dense(512, activation='relu'))
+
+model.add(Flatten())
+
 model.add(Dense(10, activation='softmax'))
 
-adam = Adam(lr=0.0006, beta_1=0.9, beta_2=0.999, decay=0.0)
+#use SGD with nesterov and momentum as optimizer
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
-#compiler parameters
-model.compile(optimizer=adam,
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=sgd)
+
 model.summary()
 
 #training Parameters
-batch_size = 32
-epochs = 15
+batch_size = 128
+epochs = 200
 
 
 history = model.fit(x=X_train,y=y_train,batch_size = batch_size,epochs=epochs, verbose=2,validation_split=0.2)
@@ -61,6 +93,8 @@ def plotLosses(history):
 
 plotLosses(history)
 
+
+
 # validate the model on test dataset to determine generalization
 score = model.evaluate(X_test, y_test, batch_size=batch_size)
-print("\nTest accuracy: %.1f%%" % (100.0 * score[1]))
+print("\nTest accuracy: %.1f%%" % (100.0 * score[1])) 
